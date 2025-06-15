4bc663a68e199cd65ec5a4bea68606a984f5709d
zanyxdev <23155424+ZanyXDev@users.noreply.github.com>
2025-06-15 12:59:44 +0300

    add property

--- src/datamanager.cpp
+++ src/datamanager.cpp
@@ -3,6 +3,8 @@
 DataManager::DataManager(QObject *parent)
     : QObject{parent}
     , m_boardModel(new BoardModel(this))
+    , m_maxColors(4)
+    , m_boardSize(8)
 {
     if (m_boardModel){
         QObject::connect(m_boardModel,&BoardModel::dataChanged,this,&DataManager::dataChanged);
@@ -14,4 +16,38 @@ DataManager::~DataManager()
     if (m_boardModel){
         m_boardModel->deleteLater();
     }
+}
+
+void DataManager::startNewGame()
+{
+    if (m_boardModel){
+        m_boardModel->clear();
+
+    }
+}
+
+int DataManager::getMaxColors() const
+{
+    return m_maxColors;
+}
+
+void DataManager::setMaxColors(int newMaxColors)
+{
+    if (m_maxColors == newMaxColors)
+        return;
+    m_maxColors = newMaxColors;
+    emit myMaxColorsChanged();
+}
+
+int DataManager::getBoardSize() const
+{
+    return m_boardSize;
+}
+
+void DataManager::setBoardSize(int newBoardSize)
+{
+    if (m_boardSize == newBoardSize)
+        return;
+    m_boardSize = newBoardSize;
+    emit myBoardSizeChanged();
 }
--- src/datamanager.h
+++ src/datamanager.h
@@ -1,24 +1,46 @@
 #pragma once
 
 #include <QObject>
-#include <QQmlEngine>
+#include <QDebug>
 #include <QtQml/qqml.h>
+#include <QVariantMap>
+#include <QVariant>
+
 #include "boardmodel.h"
 
 //"Детальное описание алгоритма смотри в тетрадке у Хуня"
+// Размерность: 8x8, 12x12, 16x16, 20x20 и 24x24.
+// Для каждой размерности поля есть три режима: 4 цвета, 6 цветов и 8 цветов. Итого 15 режимов.
 
 class DataManager : public QObject
 {
     Q_OBJECT
-    QML_ELEMENT    
+    QML_ELEMENT
     Q_PROPERTY(BoardModel* m_boardModel READ boardModel CONSTANT)
+    Q_PROPERTY(int maxColors READ getMaxColors WRITE setMaxColors NOTIFY myMaxColorsChanged)
+    Q_PROPERTY(int boardSize READ getBoardSize WRITE setBoardSize NOTIFY myBoardSizeChanged)
+
 public:
     explicit DataManager(QObject *parent = nullptr);
     ~DataManager();
+
     BoardModel* boardModel() const { return m_boardModel; }
+    Q_INVOKABLE void startNewGame();
+    int getMaxColors() const;
+    void setMaxColors(int newMaxColors);
+
+    int getBoardSize() const;
+    void setBoardSize(int newBoardSize);
 
 signals:
     void dataChanged();
+    void myMaxColorsChanged();
+
+    void myBoardSizeChanged();
+
 private:
     BoardModel* m_boardModel;
+
+    int m_maxColors;
+    int m_boardSize;
 };
